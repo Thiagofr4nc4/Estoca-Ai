6d00c91f829570fae2d5e60ceaf7eccb94370d6b
Thiagofr4nc4 <thiagofr4nc4@gmail.com>
2025-09-25 21:59:36 -0300

    Commit inicial

---
+++ src/test/java/com/Estoca_Ai/EstocaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Estoca_Ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstocaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
