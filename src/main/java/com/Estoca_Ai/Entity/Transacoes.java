675b6ebc74b30a613992e95619db64f125c3b2fb
Thiagofr4nc4 <thiagofr4nc4@gmail.com>
2025-10-04 03:26:13 -0300

    criado entidade Transacoes

---
+++ src/main/java/com/Estoca_Ai/Entity/Transacoes.java
@@ -0,0 +1,29 @@
+package com.Estoca_Ai.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Transacoes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Produto produto;
+
+    private int quantidade;
+
+    private String tipo; //Se é uma entrada ou saída de produtos
+
+    private LocalDateTime data;
+
+    private String responsavel;
+
+
+}
